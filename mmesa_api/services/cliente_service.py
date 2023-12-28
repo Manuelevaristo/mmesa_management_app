716f52e8ae074ea68a90fb78581393d3b1f12061
Manuelevaristo <findaevaristo@gmail.com>
2023-12-28 14:53:43 -0300

    CRUD do cliente completo

--- mmesa_api/services/cliente_service.py
+++ mmesa_api/services/cliente_service.py
@@ -11,3 +11,20 @@ def listar_clientes():
     clientes = cliente_model.Cliente.query.all()
 
     return clientes
+
+def listar_clientesById(id):
+    cliente = cliente_model.Cliente.query.filter_by(id=id).first()
+    return cliente
+
+def actualiza_cliente(cliente_anterior, cliente_novo):
+    cliente_anterior.name = cliente_novo.name
+    cliente_anterior.email = cliente_novo.email
+    cliente_anterior.street_address = cliente_novo.street_address
+    cliente_anterior.city_address = cliente_novo.city_address
+    cliente_anterior.state_address = cliente_novo.state_address
+    cliente_anterior.cell_contact = cliente_novo.cell_contact
+    db.session.commit()
+
+def remove_cliente(cliente):
+    db.session.delete(cliente)
+    db.session.commit()
--- mmesa_api/views/cliente_views.py
+++ mmesa_api/views/cliente_views.py
@@ -29,5 +29,43 @@ class ClienteList(Resource):
 
             x = cl.jsonify(resultado)
             return make_response(x, 201)
+
+class ClienteDetail(Resource):
+    def get(self, id):
+        cliente = cliente_service.listar_clientesById(id)
+        if cliente is None:
+            return make_response(jsonify("cliente não foi encontrado"), 400)
+
+        cl = cliente_schema.ClienteSchema()
+        return make_response(cl.jsonify(cliente), 200)
+    
+    def put(self, id):
+        cliente_bd = cliente_service.listar_clientesById(id)
+        if cliente_bd is None:
+            return make_response(jsonify("Cliente não foi encontrado"), 404)
+        cl = cliente_schema.ClienteSchema()
+        validate = cl.validate(request.json)
+        if validate:
+            return make_response(jsonify(validate), 400)
+        else:
+            name = request.json["name"]
+            email = request.json["email"]
+            cell_contact = request.json["cell_contact"]
+            street_address = request.json["street_address"]
+            city_address = request.json["city_address"]
+            state_address = request.json["state_address"]
+            novo_cliente = cliente.Cliente(name=name, email=email, cell_contact=cell_contact,
+                                           street_address=street_address, city_address=city_address, state_address=state_address)
+            cliente_service.actualiza_cliente(cliente_bd, novo_cliente)
+            cliente_atualizado = cliente_service.listar_clientesById(id)
+            return make_response(cl.jsonify(cliente_atualizado), 200)
+    def delete(self, id):
+        cliente_bd = cliente_service.listar_clientesById(id)
+        if cliente_bd is None:
+            return make_response(jsonify("cliente não encontrado"),404)
+        
+        cliente_service.remove_cliente(cliente_bd)
+        return make_response('Cliente excluido com sucesso!', 204)
     
 api.add_resource(ClienteList, '/clientes')
+api.add_resource(ClienteDetail, '/clientes/<int:id>')
